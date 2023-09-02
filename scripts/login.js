cc81e6f98e0e1aa27a9622087c0fd3a7fa4dca7b
Eynarr <135076865+Eynarr@users.noreply.github.com>
2023-09-02 15:34:33 -0500

    Initial commit

---
+++ scripts/login.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const loginForm = document.getElementById('loginForm');
+
+    // Evento para manejar el inicio de sesión
+    loginForm.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        // Obtener los valores del formulario de inicio de sesión
+        const userEmail = document.getElementById('loginEmail').value;
+        const userPassword = document.getElementById('loginPassword').value;
+        if (userEmail === 'q1@123.com' && userPassword === '123') {
+            alert('Inicio de sesión exitoso');
+            window.location.href = 'forms.html';
+        } else {
+            alert('Credenciales incorrectas');
+        }
+    });
+});
