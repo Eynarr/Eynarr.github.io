a3eee45cdfc358df6b7a515aa0b02451c2379a74
Eynarr <135076865+Eynarr@users.noreply.github.com>
2023-09-18 21:04:49 -0500

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
         // Obtener los valores del formulario de inicio de sesión
         const userEmail = document.getElementById('loginEmail').value;
         const userPassword = document.getElementById('loginPassword').value;
-        if (userEmail === 'admin' && userPassword === '123') {
+        if (userEmail === 'admin@123' && userPassword === '123') {
             alert('Login exitoso');
             window.location.href = 'forms.html';
         } else {
